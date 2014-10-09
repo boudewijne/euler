fbe3aa5ccd81024badab27aa0af44748147e0538
boudewijne <boudewijn@itpimp.nl>
2014-10-10 01:53:35 +0200

    minor code improvement

--- euler/6.py
+++ euler/6.py
@@ -1,5 +1,5 @@
 #Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
-
-sum_square=sum(list(map(lambda x:pow(x,2),range(0,101))))
-square_sum=pow(sum(range(0,101)),2)
+n=100
+sum_square=sum(list(map(lambda x:pow(x,2),range(0,n+1))))
+square_sum=pow(sum(range(0,n+1)),2)
 print square_sum-sum_square
